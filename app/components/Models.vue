99279870d4b43e37715503a5dddf5857bac80de9
HugoRCD <hugo.richard@epitech.eu>
2025-02-16 15:32:30 +0100

    up

--- app/components/Models.vue
+++ app/components/Models.vue
@@ -3,7 +3,7 @@ const { model, availableModels, isLoading } = useModels()
 </script>
 
 <template>
-  <USelect v-model="model" :items="availableModels" class="sm:w-48" :loading="isLoading">
+  <USelect v-model="model" :items="availableModels" class="sm:w-36" :loading="isLoading">
     <template #item-trailing="{ item }">
       <UBadge v-if="item.pro" color="neutral" variant="subtle" size="sm" class="rounded-full">
         Pro
